97088b19f678e38f88bba6f82897f812891fae2d
Sapna <sapna.singh9214@gmail.com>
2026-08-11 10:40:59 +0530

    CI demo: fix test and verify successful build

--- app.test.js
+++ app.test.js
@@ -1,17 +1,34 @@
 const request = require("supertest");
 const app = require("./app");
- 
+
 describe("GET /hello", () => {
- 
-    test("should return Hello World", async () => {
- 
-        const response = await request(app).get("/hello");
- 
-        expect(response.statusCode).toBe(200);
- 
-        expect(response.body.message).toBe("Hello World");
-    });
- 
+
+  test("should successfully return Hello World", async () => {
+
+    const response = await request(app).get("/hello");
+
+    expect(response.statusCode).toBe(200);
+
+    expect(response.body.message).toBe("Hello World");
+  });
+
 });
+
+
+// const request = require("supertest");
+// const app = require("./app");
+ 
+// describe("GET /hello", () => {
+ 
+//     test("should sucessfully return Hello World", async () => {
+ 
+//         const response = await request(app).get("/hello");
+ 
+//         expect(response.statusCode).toBe(200);
+ 
+//         expect(response.body.message).toBe("Hello World");
+//     });
+ 
+// });
  
 //  expect(response.body.message).toBe("Hello World"); make it to fail
